782ffed23bfa5d1c9fe7db0136403d20386f685a
VGmt <Victor.guillemot@gmail.com>
2023-06-08 11:43:00 -0400

    asdD

--- TagCounterGame/src/App.js
+++ TagCounterGame/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from 'react-native';
+import nfcManager from "react-native-nfc-manager";
 
 const App = (props) => {
+    const [hasNfc, setHasNfc] = useState(null);
+
+
+    useEffect(() => {
+        async function checkNfc() {
+            setHasNfc(await nfcManager.isSupported());
+        }
+        checkNfc();
+    }, []);
+
     return (
         <View style={styles.wrapper}>
             <Text>Hello NFC !</Text>
